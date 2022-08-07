d904231454ca0a78495f3a09f6db47746d604646
Irina <arina530@mail.ru>
2022-08-07 10:18:56 +0300

    program

--- Work/Program.cs
+++ Work/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+void SortingArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+
+string[] array1 = new string[5] {"arr", "43", "sorting", "98765", "yes"};
+string[] array2 = new string[array1.Length];
+
+SortingArray(array1, array2);
+PrintArray(array2);
